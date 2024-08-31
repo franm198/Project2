b1f9d3d8f3dde3a7b4ae38237c7130bbda850992
Franm <franmant@gmail.com>
2024-08-31 15:25:44 +0100

    Styling-partially-complete-adding-functionality-for-grid-clicks

--- Project2/src/main.ts
+++ Project2/src/main.ts
@@ -1,24 +1,43 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// import typescriptLogo from './typescript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://www.typescriptlang.org/" target="_blank">
+//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+//     </a>
+//     <h1>Vite + TypeScript</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite and TypeScript logos to learn more
+//     </p>
+//   </div>
+// `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+
+// Variables \\
+
+let currentPlayer = 'X'; //The Game always begins with Player X
+let gameBoard = ['', '', '', '', '', '', '', '', '']; //This is the 3x3 grid squares
+let gameState = true;
+
+// Player Turns - Handling which player is active \\
+
+function handlePlayerTurn(clickedCellIndex) {
+  if (gameBoard[clickedCellIndex] !== '' || !gameState) {
+    return;
+  }
+  gameBoard[clickedCellIndex] = currentPlayer;
+  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+}
+
+// 
