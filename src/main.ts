96fb9d3ab3b4f7d0b6cc4d8ed8475bdd83fa1909
Franm <franmant@gmail.com>
2024-09-03 12:32:13 +0100

    fixed-typescript-added-win-logic

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,14 @@
-import './style.css'
-// import typescriptLogo from './typescript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.ts'
-
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>Vite + TypeScript</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite and TypeScript logos to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
-
+import './style.css';
 
 // Variables \\
 
-let currentPlayer = 'X'; //The Game always begins with Player X
-let gameBoard = ['', '', '', '', '', '', '', '', '']; //This is the 3x3 grid squares
+let currentPlayer: string = 'X'; //The Game always begins with Player X
+let gameBoard: string[] = ['', '', '', '', '', '', '', '', '']; //This is the 3x3 grid squares
 let gameStateActive = true;
 
 // Player Turns - Handling which player is active \\
 
-function handlePlayerTurn(clickedGridSquareIndex) {
+function handlePlayerTurn(clickedGridSquareIndex: number) {
   if (gameBoard[clickedGridSquareIndex] !== '' || !gameStateActive) {
     return;
   }
@@ -42,17 +18,33 @@ function handlePlayerTurn(clickedGridSquareIndex) {
 
 // Event Listeners - Adding one for each cell \\
 
-const gridSquare = document.querySelectorAll('.gridSquare');
+const gridSquare = document.querySelectorAll<HTMLDivElement> ('.gridSquare');
+
+const messageElement = document.getElementById('gameMessage');
+
+if (!gridSquare || !messageElement)
+  {throw new Error('error with retrieving grid square');
+};
 
 gridSquare.forEach(gridSquare => {
-  gridSquare.addEventListener('click', gridSquareClicked, false)
+  gridSquare.addEventListener('click', gridSquareClicked);
 });
+
+
+const resetGame = () => {
+  location.reload()
+}
+
+const resetButton = document.getElementById('resetButton');
+resetButton?.addEventListener('click', resetGame, false);
 
 // Grid Square Clicks - Handling when the cell is clicked \\
 
-function gridSquareClicked(clickedGridEvent) {
-  const clickedGridSquare = clickedGridEvent.target;
-  const clickedGridSquareIndex = parseInt(clickedGridSquare.id.replace('cell-', '')) - 1;
+function gridSquareClicked(clickedGridEvent: Event) {
+  const clickedGridSquare = clickedGridEvent.currentTarget as HTMLDivElement;
+  console.log('event listener working');
+  console.log(clickedGridSquare);
+  const clickedGridSquareIndex = parseInt(clickedGridSquare.id.replace('square-', '')) - 1;
 
     if (gameBoard[clickedGridSquareIndex] !== '' || !gameStateActive) {
       return;
@@ -64,7 +56,80 @@ function gridSquareClicked(clickedGridEvent) {
 }
 
 function updateUI() {
+  console.log('updating UI');
   for (let i=0; i < gridSquare.length; i++) {
     gridSquare[i].innerText = gameBoard[i];
   }
+};
+
+// Win Logic - Declaring game state and who has won. 
+
+// Variables - 
+
+const winningConditions = [
+  [0, 1, 2], //Top row
+  [3, 4, 5], //Middle row
+  [6, 7, 8], //Bottom row
+  [0, 3, 6], //Left column
+  [1, 4, 7], //Middle column
+  [2, 5, 8], //Right column
+  [0, 4, 8], //Left-to-right diagonal
+  [2, 4, 6] //Right-to-left diagonal
+];
+
+let someoneHasLostOrWon = false
+
+// Function to Check whether it's win or a draw
+
+function announceWinner(player:string) {
+  // const messageElement = document.getElementById('gameMessage');
+  if(messageElement && someoneHasLostOrWon === false){
+    messageElement.innerText = `Player ${player} Wins`;
+    someoneHasLostOrWon = true
+  }
 }
+
+function announceDraw() {
+  // const messageElement = document.getElementById('gameMessage');
+  if(messageElement && someoneHasLostOrWon === false){
+    messageElement.innerText = 'Game Draw!';
+    someoneHasLostOrWon = true
+  }
+};
+
+function checkForWinOrDraw() {
+  let roundWon = false;
+
+  for (let i = 0; i < winningConditions.length; i++) {
+    const [a, b, c] = winningConditions[i];
+    if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+      roundWon = true;
+      break;
+    }
+  }
+
+  if (roundWon) {
+    if(currentPlayer === "X"){
+      currentPlayer = "O"
+    }
+    else if(currentPlayer === "O"){
+      currentPlayer = "X"
+    }
+
+    announceWinner(currentPlayer);
+    gameStateActive = false;
+    return;
+  }
+
+  let roundDraw = !gameBoard.includes('');
+  if (roundDraw) {
+    announceDraw();
+    gameStateActive = false;
+    return;
+  }
+}
+
+gridSquare.forEach(gridSquare => {
+  gridSquare.addEventListener('click', checkForWinOrDraw);
+});
+
